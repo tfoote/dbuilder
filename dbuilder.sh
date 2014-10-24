820b464c6be2051fbbb6ff4bb4e1d2c5e71a2b4e
Tully Foote <tfoote@osrfoundation.org>
2014-10-24 14:31:55 -0700

    functional retimpstamping in the debbuild

--- dbuilder.sh
+++ dbuilder.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
 
+set -o errexit
+
 PACKAGE=$1
 
 echo dbuilder building package: $PACKAGE
 apt-get update
-apt-src install $PACKAGE
-apt-src build $PACKAGE
+
+
+# Not working for ROS packages trying to install fully versioned sourcedeb
+# apt-src install $PACKAGE -t
+# alternative below
+echo apt-get source $PACKAGE
+apt-get source $PACKAGE
+
+echo apt-get build-dep -y $PACKAGE
+apt-get build-dep -y $PACKAGE
+
+# TODO do this better
+cd */.
+
+apt-get install -y devscripts
+prevversion=`dpkg-parsechangelog | grep Version | awk '{print \$2}'`
+newversion=$prevversion-`date +%Y%m%d-%H%M-%z`
+debchange -D $(lsb_release -sc) -v $newversion 'Time stamping.'
+
+
+
+echo apt-src import $PACKAGE --location .
+apt-src import $PACKAGE --location . -t --version $newversion
+echo apt-src build $PACKAGE
+apt-src build $PACKAGE -t
+
+cp -r .. /output
